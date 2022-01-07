4b7d895af8ecf86a4544ab3d4254657f2af5e8a9
Christian Ramseyer <ramseyer@netnea.com>
2022-01-08 00:41:55 +0100

    update 1

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from waitress import serve
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World (forked)!"
+    out = ["Hello World (forked)!"]
+    for k, v in os.environ.items():
+        out.append(f'{k}={v}')
 
+    return str.join("<br>\n", out)
+
+
+# to run: FLASK_APP=app python -m flask run
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
